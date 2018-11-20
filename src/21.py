85e9af00c94414a8541d2013a24d0fe73ae4ecb2
Yaqiao <liu.yaqi@husky.neu.edu>
2018-11-20 16:44:54 +0800

    Linked List

---
+++ src/21.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        
+        head = ListNode(-1)
+        prev = head
+        curt1 = l1
+        curt2 = l2
+        
+        while l1 != None and l2 != None:
+            if l1.val < l2.val:
+                head.next = l1
+                head = head.next
+                l1 = l1.next                
+            else:
+                head.next = l2
+                head = head.next
+                l2 = l2.next
+        
+        if l1 == None:
+            head.next = l2
+        if l2 == None:
+            head.next = l1
+        
+        return prev.next
+        
+        
