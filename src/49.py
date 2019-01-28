134a233d19c991f19e4f58ff5391a8ae09742da8
Yaqiao <liu.yaqi@husky.neu.edu>
2019-01-28 14:27:12 -0500

    ana

--- src/49.py
+++ src/49.py
@@ -22,3 +22,25 @@ class Solution(object):
                 res.append(new)
                 hashMap[s] = index
         return res
+
+
+'''
+这个题要求我们将list中的字符串按照各自包含的char的不同而分开；
+
+这种题我们很自然的想到要使用hashmap的方法，所以难点再也我们如
+何去构造hashmap；
+
+这里用的一种方法是，不同的string排序之后得到的string是相同的，
+所以我们可以利用这个性质，巧妙的是，它的key值是当前储存strings
+的数组在res中的index。
+
+这里一个比较常用的hashmap查找的结构是：
+
+if s in hashMap:
+    '''如果找到进行的操作'''
+else:
+    '''如果找不到之后进行的操作'''
+
+同时这种hashmap中value映射的方法也要多积累注意；
+
+'''
